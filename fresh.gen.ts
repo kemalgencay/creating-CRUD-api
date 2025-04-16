29e681ef2332fc4ac743f0323aa6f03ac244e1af
kemalgencay <kemalgencay@gmail.com>
2025-04-16 13:01:18 +0300

    With errors

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,7 @@ import * as $_id_ from "./routes/[id].ts";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
+import * as $api_users from "./routes/api/users.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
@@ -17,6 +18,7 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
+    "./routes/api/users.tsx": $api_users,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
